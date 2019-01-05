68ae7ce109e36b2c453e0636117a9953b809fbd1
misinayu <yama_masa22@yahoo.co.jp>
2019-01-05 21:08:43 +0900

    Override toString function in Rational class

--- HelloWorld/src/Rational.kt
+++ HelloWorld/src/Rational.kt
@@ -3,7 +3,10 @@ package sample
 fun main(args: Array<String>) {
     val half = Rational(1, 2)
     println(half)
-    println(half.denominator)
+    val half2 = Rational(2, 5)
+    println(half2)
 }
 
-class Rational(val numerator: Int, val denominator: Int)
+class Rational(val numerator: Int, val denominator: Int) {
+    override fun toString(): String = "${numerator}/${denominator}"
+}
