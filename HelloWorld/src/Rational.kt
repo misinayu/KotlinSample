a00a322b2c3f40972a1f2c4a6d751d041eb953a2
misinayu <yama_masa22@yahoo.co.jp>
2019-01-06 21:05:04 +0900

    Modified toString

--- HelloWorld/src/Rational.kt
+++ HelloWorld/src/Rational.kt
@@ -1,9 +1,9 @@
 package sample
 
 fun main(args: Array<String>) {
-    val half = Rational(1, 0)
+    val half = Rational(55, 100)
     println(half)
-    val half2 = Rational(2, 5)
+    val half2 = Rational(2, 6)
     println(half2)
 }
 
@@ -11,5 +11,13 @@ class Rational(val numerator: Int, val denominator: Int) {
     init {
         require(denominator != 0, {"denominator must not be null"})
     }
-    override fun toString(): String = "${numerator}/${denominator}"
+
+    private val g = gcd(Math.abs(numerator), Math.abs(denominator))
+    val n: Int = numerator / g
+    val d: Int = denominator / g
+    override fun toString(): String = "${n}/${d}"
+
+    tailrec private fun gcd(a: Int, b: Int): Int =
+        if (b == 0) a
+        else gcd(b, a % b)
 }
