e2ea0c1b7fe223df77d18377d6f986eaffe913a7
misinayu <yama_masa22@yahoo.co.jp>
2019-01-06 20:49:13 +0900

    Create Initializer in Rational class

--- HelloWorld/src/Rational.kt
+++ HelloWorld/src/Rational.kt
@@ -1,12 +1,15 @@
 package sample
 
 fun main(args: Array<String>) {
-    val half = Rational(1, 2)
+    val half = Rational(1, 0)
     println(half)
     val half2 = Rational(2, 5)
     println(half2)
 }
 
 class Rational(val numerator: Int, val denominator: Int) {
+    init {
+        require(denominator != 0, {"denominator must not be null"})
+    }
     override fun toString(): String = "${numerator}/${denominator}"
 }
