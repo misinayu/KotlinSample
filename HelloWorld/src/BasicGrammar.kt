8debcad97034f777435c3854a3305bbd4ce33d9e
misinayu <yama_masa22@yahoo.co.jp>
2019-01-08 21:25:53 +0900

    Lesson Set

--- HelloWorld/src/BasicGrammar.kt
+++ HelloWorld/src/BasicGrammar.kt
@@ -1,24 +1,13 @@
 package sample
 
 fun main(args: Array<String>) {
-    // イミュターブルリスト
-    val ints: List<Int> = listOf<Int>(1, 2, 3)
+    // イミュターブルセット
+    val ints: Set<Int> = setOf(1, 2, 1, 3)
     println(ints)
-    println(ints.size)
-    println(ints[0])
-    // error:: ints[0] = 5
-
-    // MutableList（変更可能なリスト）
-    val char: MutableList<Char> = mutableListOf('a', 'b')
-    println(char)
-    println(char[0])
-    char[0] = 'c'
-    println(char)
-
-    char += 'X'
-    println(char)
-    char -= 'X'
-    println(char)
-    char.removeAt(0)
-    println(char)
+    // ミュータブルセット
+    val chars: MutableSet<Char> = mutableSetOf('a', 'a', 'b')
+    // Setは要素の重複がない
+    println(chars)
+    chars -= 'a'
+    println(chars)
 }
