8b26842f3080d787727f5cf03901f29b652cd6a5
misinayu <yama_masa22@yahoo.co.jp>
2019-01-11 22:49:02 +0900

    Lesson If else

--- HelloWorld/src/BasicGrammar.kt
+++ HelloWorld/src/BasicGrammar.kt
@@ -1,13 +1,13 @@
 package sample
 
 fun main(args: Array<String>) {
-    // range(レンジ)
-    val range: IntRange = 12..15
-    println(5 in range)
-    println(5 !in range)
-    println(range.toList())
-    println(range.reversed().toList())
-    println((5 downTo 1).toList())
-    println((1..5 step 2).toList())
-    println((100 downTo 0 step 25).toList())
+    // if文
+    val score = 75
+    val grade =
+        if (score >= 90) 'A'
+        else if (score >= 80) 'B'
+        else if (score >= 70) 'C'
+        else if (score >= 60) 'D'
+        else 'F'
+    println(grade)
 }
