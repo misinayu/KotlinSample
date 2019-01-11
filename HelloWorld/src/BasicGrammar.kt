d2426909dc357028356c09b3d227313cba9d5ff9
misinayu <yama_masa22@yahoo.co.jp>
2019-01-11 23:05:49 +0900

    Lesson When

--- HelloWorld/src/BasicGrammar.kt
+++ HelloWorld/src/BasicGrammar.kt
@@ -1,13 +1,46 @@
 package sample
 
 fun main(args: Array<String>) {
-    // if文
-    val score = 75
-    val grade =
-        if (score >= 90) 'A'
-        else if (score >= 80) 'B'
-        else if (score >= 70) 'C'
-        else if (score >= 60) 'D'
-        else 'F'
-    println(grade)
+    // when式
+    // Javaにおけるswitch文を強力にしたようなもの
+    val x = 22
+    val result = when (x) {
+        1 -> "one"
+        2, 3 -> "two or three"
+        else -> {
+            "unknown"
+        }
+    }
+    println(result)
+
+    // 分岐条件の部分に、定数以外を指定できる
+    val result2 = when (x) {
+        1 -> "one"
+        myFavoriteInt() -> "favorite: $x"
+        in 2..10 -> "1 <= x <= 10"
+        else -> x.toString()
+    }
+    println(result2)
+
+    // 型チェックで分岐
+    val msg = "test"
+    val blank = when (msg) {
+        is String -> msg.isBlank()
+        else -> true
+    }
+    println(blank)
+
+    val score = 69
+    val scorere = when {
+        score >= 90 -> 'A'
+        score >= 80 -> 'B'
+        score >= 70 -> 'C'
+        score >= 60 -> 'D'
+        else ->'F'
+    }
+    println(scorere)
+}
+
+fun myFavoriteInt(): Int {
+    return 22
 }
