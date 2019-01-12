e6b2196aefc2dd823a448db743c5c72eab75c558
misinayu <yama_masa22@yahoo.co.jp>
2019-01-12 23:54:02 +0900

    Lesson While

--- HelloWorld/src/BasicGrammar.kt
+++ HelloWorld/src/BasicGrammar.kt
@@ -1,46 +1,14 @@
 package sample
 
 fun main(args: Array<String>) {
-    // when式
-    // Javaにおけるswitch文を強力にしたようなもの
-    val x = 22
-    val result = when (x) {
-        1 -> "one"
-        2, 3 -> "two or three"
-        else -> {
-            "unknown"
-        }
+    // while文
+    var count = 3
+    while (count-- > 0) {
+        println("Hello")
     }
-    println(result)
 
-    // 分岐条件の部分に、定数以外を指定できる
-    val result2 = when (x) {
-        1 -> "one"
-        myFavoriteInt() -> "favorite: $x"
-        in 2..10 -> "1 <= x <= 10"
-        else -> x.toString()
-    }
-    println(result2)
-
-    // 型チェックで分岐
-    val msg = "test"
-    val blank = when (msg) {
-        is String -> msg.isBlank()
-        else -> true
-    }
-    println(blank)
-
-    val score = 69
-    val scorere = when {
-        score >= 90 -> 'A'
-        score >= 80 -> 'B'
-        score >= 70 -> 'C'
-        score >= 60 -> 'D'
-        else ->'F'
-    }
-    println(scorere)
-}
-
-fun myFavoriteInt(): Int {
-    return 22
+    do {
+        println("do while")
+        val next = Math.random() < 0.5
+    } while (next)
 }
