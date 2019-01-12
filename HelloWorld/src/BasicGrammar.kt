ce135de7e06174110fe98fd4e7b296626ceb09c9
misinayu <yama_masa22@yahoo.co.jp>
2019-01-13 00:05:19 +0900

    Lesson For loop

--- HelloWorld/src/BasicGrammar.kt
+++ HelloWorld/src/BasicGrammar.kt
@@ -1,14 +1,53 @@
 package sample
 
 fun main(args: Array<String>) {
-    // while文
-    var count = 3
-    while (count-- > 0) {
-        println("Hello")
+    // forループ
+    for (x in arrayOf(1, 2, 3)) {
+        println(x)
     }
 
-    do {
-        println("do while")
-        val next = Math.random() < 0.5
-    } while (next)
+    val names = listOf("foo", "bar", "bax")
+    for (name in names) {
+        println(name)
+    }
+
+    for (i in 1..10 step 2) {
+        println(i)
+    }
+
+    // Iterator
+    for (item in MyIterable()) {
+        println(item)
+    }
+
+    // break
+    var barIndex = -1
+    for (index in names.indices) {
+        if (names[index] == "bar") {
+            barIndex = index
+            println(barIndex)
+            break
+
+        }
+    }
+
+    // label
+    println("label----------------------")
+    loop@ for (x in 1..10) {
+        for (y in 1..10) {
+            println(x + y)
+            if (y == 5) {
+                break@loop
+            }
+        }
+    }
+}
+
+class MyIterator {
+    operator fun hasNext(): Boolean = Math.random() < 0.5
+    operator fun next(): String = "Hello"
+}
+
+class MyIterable {
+    operator fun iterator() = MyIterator()
 }
