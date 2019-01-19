8ce40b4d35fbeeecd3b1550d9beb69e8969e2f0a
misinayu <yama_masa22@yahoo.co.jp>
2019-01-19 23:37:46 +0900

    Lesson tailrec

--- HelloWorld/src/Function.kt
+++ HelloWorld/src/Function.kt
@@ -1,15 +1,18 @@
 package sample
 
+import java.util.concurrent.atomic.LongAccumulator
+
 fun main(args: Array<String>) {
     println(succ(32))
 //    println(sum(arrayOf(1, 2, 3)))
     println(sub(5, 3))
     println(hello())
     println(hello("Alice"))
-    println(sum(1, 2, 3))
-    println(sum(*intArrayOf(1, 2, 3)))
-    val ints = intArrayOf(2, 3, 4)
-    println(sum(*ints))
+//    println(sum(1, 2, 3))
+//    println(sum(*intArrayOf(1, 2, 3)))
+//    val ints = intArrayOf(2, 3, 4)
+//    println(sum(*ints))
+    println(sum((1L..123456).toList()))
 }
 
 fun succ(i: Int): Int = i + 1
@@ -27,10 +30,14 @@ fun sub(minuend: Int, subtrahend: Int): Int =
 
 fun hello(name: String = "World"): String = "Hello, $name!"
 
-fun sum(vararg ints: Int): Int {
-    var sum = 0
-    for (i in ints) {
-        sum += i
-    }
-    return sum
-}
+//fun sum(vararg ints: Int): Int {
+//    var sum = 0
+//    for (i in ints) {
+//        sum += i
+//    }
+//    return sum
+//}
+
+tailrec fun sum(numbers: List<Long>, accumulator: Long = 0): Long =
+        if (numbers.isEmpty()) accumulator
+        else sum(numbers.drop(1), accumulator + numbers.first())
