27ba786a27129e6352f7fd94d02840afeb5c9954
misinayu <yama_masa22@yahoo.co.jp>
2019-01-19 23:19:49 +0900

    Lesson Function 2

--- HelloWorld/src/Function.kt
+++ HelloWorld/src/Function.kt
@@ -2,6 +2,23 @@ package sample
 
 fun main(args: Array<String>) {
     println(succ(32))
+    println(sum(arrayOf(1, 2, 3)))
+    println(sub(5, 3))
+    println(hello())
+    println(hello("Alice"))
 }
 
 fun succ(i: Int): Int = i + 1
+
+fun sum(ints: Array<Int>): Int {
+    var sum = 0
+    for (i in ints) {
+        sum += i
+    }
+    return sum
+}
+
+fun sub(minuend: Int, subtrahend: Int): Int =
+        minuend - subtrahend
+
+fun hello(name: String = "World"): String = "Hello, $name!"
