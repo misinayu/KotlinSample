84eb9b722b625f6494ee6cdb6e127edbde6c28ad
misinayu <yama_masa22@yahoo.co.jp>
2019-01-20 22:33:35 +0900

    Lesson Higher-order function

--- HelloWorld/src/FirstClassObject.kt
+++ HelloWorld/src/FirstClassObject.kt
@@ -3,6 +3,49 @@ package sample
 fun main(args: Array<String>) {
     val functionObject = ::square
     println(functionObject(5))
+    println(firstK2("kakakakKwowKKow"))
+    println(firstUpperCase2("niniCoCo"))
 }
 
 fun square(i: Int): Int = i * i
+
+fun firstK(str: String): Int {
+    tailrec fun go(str: String, index: Int): Int =
+            when {
+                str.isEmpty() -> -1
+                str.first() == 'K' -> index
+                else -> go(str.drop(1), index + 1)
+            }
+    return go(str, 0)
+}
+
+fun firstUpperCase(str: String): Int {
+    tailrec fun go(str: String, index: Int): Int =
+            when {
+                str.isEmpty() -> -1
+                str.first().isUpperCase() -> index
+                else -> go(str.drop(1), index + 1)
+            }
+    return go(str, 0)
+}
+
+// firstK() と firstUpperCase() を共通化した関数
+fun first(str: String, predicate: (Char) -> Boolean): Int {
+    tailrec fun go(str: String, index: Int): Int =
+            when {
+                str.isEmpty() -> -1
+                predicate(str.first()) -> index
+                else -> go(str.drop(1), index + 1)
+            }
+    return go(str, 0)
+}
+
+fun firstK2(str: String): Int {
+    fun  isK(c: Char): Boolean = c == 'K'
+    return first(str, ::isK)
+}
+
+fun firstUpperCase2(str: String): Int {
+    fun isUpperCase(c: Char): Boolean = c.isUpperCase()
+    return first(str, ::isUpperCase)
+}
