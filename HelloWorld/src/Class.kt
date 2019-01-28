31b01cdcda25861ed75b25385b09a73558982530
misinayu <yama_masa22@yahoo.co.jp>
2019-01-28 22:23:03 +0900

    Lesson Method in Class

--- HelloWorld/src/Class.kt
+++ HelloWorld/src/Class.kt
@@ -3,6 +3,15 @@ package sample
 fun main(args: Array<String>) {
     val myClass: MyClass = MyClass()
     println(myClass)
+
+    val greeter = Greeter()
+    greeter.greet("Kotlin")
 }
 
 class MyClass
+
+class Greeter {
+    fun greet(name: String) {
+        println("Hello, $name")
+    }
+}
