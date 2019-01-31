458f478fcb02f8aecf0c54081410d77806702a3e
misinayu <yama_masa22@yahoo.co.jp>
2019-01-31 22:48:34 +0900

    Lesson Inheritance

--- HelloWorld/src/InheritanceAndAbstract.kt
+++ HelloWorld/src/InheritanceAndAbstract.kt
@@ -1,2 +1,19 @@
 package sample
 
+fun main(args: Array<String>) {
+    val person: Person1 = Person1("ゆたか")
+    person.introduceMyself()
+
+    val student: Student = Student("くみこ", 123)
+    println(student.id)
+    println(student.name)
+    student.introduceMyself()
+}
+
+open class Person1(val name: String) {
+    fun introduceMyself() {
+        println("I am $name.")
+    }
+}
+
+class Student(name: String, val id: Long): Person1(name)
