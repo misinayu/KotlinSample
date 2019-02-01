1a055308eb03cf6bf409c6d69c61af4cd9bb5e38
misinayu <yama_masa22@yahoo.co.jp>
2019-02-01 22:55:16 +0900

    Lesson Abstract Member

--- HelloWorld/src/InheritanceAndAbstract.kt
+++ HelloWorld/src/InheritanceAndAbstract.kt
@@ -8,6 +8,10 @@ fun main(args: Array<String>) {
     println(student.id)
     println(student.name)
     student.introduceMyself()
+
+    EnglishGreeter("Kotlin").sayHello()
+
+    JapaneseGreeter("Java").sayHello()
 }
 
 open class Person1(open val name: String) {
@@ -20,5 +24,21 @@ class Student(override val name: String, val id: Long): Person1(name) {
     override fun introduceMyself() {
         super.introduceMyself()
         println("I am $name(id=$id)")
+    }
+}
+
+abstract class AbstractGreeter(val target: String) {
+    abstract fun sayHello()
+}
+
+class EnglishGreeter(target: String) : AbstractGreeter(target) {
+    override fun sayHello() {
+        println("Hello, $target!")
+    }
+}
+
+class JapaneseGreeter(target: String) : AbstractGreeter(target) {
+    override fun sayHello() {
+        println("こんにちは、$target！")
     }
 }
