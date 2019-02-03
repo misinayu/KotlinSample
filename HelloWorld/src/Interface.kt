c25cbd1ac93e22249133e914e3a27921c2b3aae1
misinayu <yama_masa22@yahoo.co.jp>
2019-02-03 22:40:29 +0900

    Lesson Interface

--- HelloWorld/src/Interface.kt
+++ HelloWorld/src/Interface.kt
@@ -3,3 +3,22 @@ package sample
 fun main(args: Array<String>) {
 
 }
+
+interface InterfaceGreeter {
+    val language: String
+    fun sayHello(target: String)
+}
+
+class EnglishGreeterInter : InterfaceGreeter {
+    override val language = "English"
+    override fun sayHello(target: String) {
+        println("Hello, $target")
+    }
+}
+
+open class Superclass
+
+interface Foo
+interface Bar
+
+class MyInterClass: Superclass(), Foo, Bar
