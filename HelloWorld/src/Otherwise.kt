0dfdeb7a4f30d047019c289e12a75c87cc90bee0
misinayu <yama_masa22@yahoo.co.jp>
2019-02-23 15:45:55 +0900

    Algebraic data type

--- HelloWorld/src/Otherwise.kt
+++ HelloWorld/src/Otherwise.kt
@@ -69,6 +69,12 @@ fun main(args: Array<String>) {
     val dummy = Usercom.Pool.DUMMY
     println("${dummy.id}, ${dummy.name}")
     println(Usercom.DUMMY.name)
+
+    // 代数的データ型
+    println(Cons("foo", Cons("bar", Cons("baz", Nil))))
+
+    println(headString(Cons("foo", Nil)))
+    println(headString(Nil))
 }
 
 // 演算子オーバーロード
@@ -114,3 +120,19 @@ class Usercom(val id: Long, val name: String) {
         val DUMMY = Usercom(0, "dummy")
     }
 }
+
+interface MyList<out T>
+
+object Nil: MyList<Nothing> {
+    override fun toString(): String = "Nil"
+}
+
+class Cons<T>(val head: T, val tail: MyList<T>): MyList<T> {
+    override fun toString(): String = "$head:$tail"
+}
+
+fun headString(list: MyList<*>): String =
+        when (list) {
+            is Cons<*> -> list.head.toString()
+            else -> "要素なし"
+        }
