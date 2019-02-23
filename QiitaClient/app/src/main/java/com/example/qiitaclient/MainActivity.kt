856eada69dc4892a205b9ae33856c855cd4d3cad
misinayu <yama_masa22@yahoo.co.jp>
2019-02-23 17:46:55 +0900

    Add CreateView class
    impliment

--- QiitaClient/app/src/main/java/com/example/qiitaclient/MainActivity.kt
+++ QiitaClient/app/src/main/java/com/example/qiitaclient/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.qiitaclient
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.qiitaclient.model.Article
+import com.example.qiitaclient.model.User
+import com.example.qiitaclient.view.ArticleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // ArticleViewオブジェクトを生成
+        val articleView = ArticleView(applicationContext)
+
+        // Articleオブジェクトを生成して、ArticleViewオブジェクトにセット
+        articleView.setArticle(Article(id = "123",
+            title = "Kotlin入門",
+            url = "http://www.example.com/articles/123",
+            user = User(id = "456", name = "たろう", profileImageUrl = "")))
+
+        // このアクティビティにArticleViewオブジェクトをセット
+        setContentView(articleView)
     }
 }
---
+++ QiitaClient/app/src/main/java/com/example/qiitaclient/view/ArticleView.kt
@@ -0,0 +1,49 @@
+package com.example.qiitaclient.view
+
+import android.content.Context
+import android.graphics.Color
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.qiitaclient.R
+import com.example.qiitaclient.model.Article
+
+class ArticleView : FrameLayout {
+
+    constructor(context: Context?) : super(context)
+
+    constructor(context: Context?,
+                attrs: AttributeSet?) : super(context, attrs)
+
+    constructor(context: Context?,
+                attrs: AttributeSet?,
+                defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    constructor(context: Context?,
+                attrs: AttributeSet?,
+                defStyleAttr: Int,
+                defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
+
+    var profileImageView: ImageView? = null
+
+    var titleTextView: TextView? = null
+
+    var userNameTextView: TextView? = null
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.view_article, this)
+        profileImageView = findViewById(R.id.profile_image_view) as ImageView
+        titleTextView = findViewById(R.id.title_text_view) as TextView
+        userNameTextView = findViewById(R.id.user_name_text_view) as TextView
+    }
+
+    fun setArticle(article: Article) {
+        titleTextView?.text = article.title
+        userNameTextView?.text = article.user.name
+
+        // TODO プロフィール画像をセットする
+        profileImageView?.setBackgroundColor(Color.RED)
+    }
+}
