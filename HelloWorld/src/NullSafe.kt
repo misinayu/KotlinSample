db2dd94d8c6862640e53fc8b8708e0b4f727f9b0
misinayu <yama_masa22@yahoo.co.jp>
2019-02-23 12:20:21 +0900

    Lesson Smart Cast

--- HelloWorld/src/NullSafe.kt
+++ HelloWorld/src/NullSafe.kt
@@ -3,4 +3,25 @@ package sample
 fun main(args: Array<String>) {
     val s: String? = null
     println(s)
+
+    val a: String? = null
+    val b: String? = "Hello"
+
+    if (a != null) {
+        println(a.toUpperCase())
+    }
+    if (b != null) {
+        println(b.toUpperCase())
+    }
+
+    val list: List<Any> = listOf(1, 'a', false)
+    for(e in list) {
+        val result: Any? = when (e) {
+            is Int -> e + 5
+            is Char -> e.toUpperCase()
+            is Boolean -> e.not()
+            else -> null
+        }
+        println(result)
+    }
 }
