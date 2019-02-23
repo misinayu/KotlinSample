5a1653e29f009a462e7c6a3f8127de9b4e1cc4c4
misinayu <yama_masa22@yahoo.co.jp>
2019-02-23 11:38:29 +0900

    Lesson Generics

--- HelloWorld/src/Generics.kt
+++ HelloWorld/src/Generics.kt
@@ -1,5 +1,21 @@
 package sample
 
 fun main(args: Array<String>) {
+//    val intContainer = Container(123)
+//    val i: Int = intContainer.value as Int
+//    println(i)
+//
+//    val strContainer = Container("Hello")
+//    val s = strContainer.value as String
+//    println(s)
 
+    val intContainer: Container<Int> = Container<Int>(123)
+    val i: Int = intContainer.value
+    println(i)
+
+    val strContainer = Container("Hello")
+    println(strContainer.value.toUpperCase())
 }
+
+//class Container(var value: Any)
+class Container<T>(var value: T)
