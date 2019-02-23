e018a74cef77a7551718ff3ae23db8c0eeefabb7
misinayu <yama_masa22@yahoo.co.jp>
2019-02-23 14:46:24 +0900

    Lesson Equivalence

--- HelloWorld/src/Otherwise.kt
+++ HelloWorld/src/Otherwise.kt
@@ -9,14 +9,23 @@ fun main(args: Array<String>) {
     println(service())
     println(service('B'))
     println(service.invoke('C'))
+
+    println(MyInt(2) == MyInt(2))
+    println(MyInt(1) == MyInt(3))
+    println(MyInt(1) != MyInt(3))
 }
 
+// 演算子オーバーロード
 class MyInt(val value: Int) {
     operator fun times(that: MyInt): MyInt =
             MyInt(value * that.value)
 
     operator fun rem(that: MyInt): MyInt =
             MyInt(value % that.value)
+
+    override fun equals(other: Any?): Boolean =
+            if (other is MyInt) value == other.value
+            else false
 }
 
 class Service {
