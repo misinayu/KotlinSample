d6d099f72ba73a5ded8d9a24e591f0da16dce376
misinayu <yama_masa22@yahoo.co.jp>
2019-03-16 22:28:14 +0900

    Create ArticleListAdapter.kt

---
+++ QiitaClient/app/src/main/java/com/example/qiitaclient/ArticleListAdapter.kt
@@ -0,0 +1,8 @@
+package com.example.qiitaclient
+
+import android.content.Context
+import android.widget.BaseAdapter
+
+class ArticleListAdapter(private val context: Context) : BaseAdapter() {
+    
+}
