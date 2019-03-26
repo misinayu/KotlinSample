a5f1e8311e4ddb8c756e7a7c009bac0bdb9833aa
misinayu <yama_masa22@yahoo.co.jp>
2019-03-26 21:01:09 +0900

    Make search button

--- QiitaClient/app/src/main/java/com/example/qiitaclient/MainActivity.kt
+++ QiitaClient/app/src/main/java/com/example/qiitaclient/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.qiitaclient
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ListView
 import com.example.qiitaclient.model.Article
 import com.example.qiitaclient.model.User
 import com.example.qiitaclient.view.ArticleView
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,6 +31,32 @@ class MainActivity : AppCompatActivity() {
         listView.setOnItemClickListener { adapterView, view, position, id ->
             val article = listAdapter.articles[position]
             ArticleActivity.intent(this, article).let { startActivity(it) }
+        }
+
+        val gson = GsonBuilder()
+            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+            .create()
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://qiita.com")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+            .build()
+        val articleClient = retrofit.create(ArticleClient::class.java)
+
+        val queryEditText = findViewById(R.id.query_edit_text) as EditText
+        val searchButton = findViewById(R.id.search_button) as Button
+
+        searchButton.setOnClickListener {
+            articleClient.search(queryEditText.text.toString())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    queryEditText.text.clear()
+                    listAdapter.articles = it
+                    listAdapter.notifyDataSetChanged()
+                }, {
+                    toast("エラー： $it")
+                })
         }
     }
 
