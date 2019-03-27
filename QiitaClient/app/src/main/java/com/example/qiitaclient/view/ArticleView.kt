9dffae9dda6b79485968a13637b261cf29fdd2f9
misinayu <yama_masa22@yahoo.co.jp>
2019-03-27 23:07:21 +0900

    Add glide

--- QiitaClient/app/src/main/java/com/example/qiitaclient/view/ArticleView.kt
+++ QiitaClient/app/src/main/java/com/example/qiitaclient/view/ArticleView.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.example.qiitaclient.R
 import com.example.qiitaclient.model.Article
 import com.example.qiitaclient.bindView
@@ -40,8 +41,6 @@ class ArticleView : FrameLayout {
     fun setArticle(article: Article) {
         titleTextView.text = article.title
         userNameTextView.text = article.user.name
-
-        // TODO プロフィール画像をセットする
-        profileImageView.setBackgroundColor(Color.RED)
+        Glide.with(context).load(article.user.profileImageUrl).into(profileImageView)
     }
 }
