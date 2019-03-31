eee96129ec4b50fa5f98cb5f924e48ee01870ea4
misinayu <yama_masa22@yahoo.co.jp>
2019-03-31 22:37:19 +0900

    Ready Test

---
+++ QiitaClient/app/src/androidTest/java/com/example/qiitaclient/MainActivityTest.kt
@@ -0,0 +1,13 @@
+package com.example.qiitaclient
+
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+    @Test
+    fun test() {
+        throw AssertionError("失敗するはず")
+    }
+}
