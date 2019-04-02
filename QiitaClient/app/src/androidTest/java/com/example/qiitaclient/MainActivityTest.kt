6f4924e82659a930f1b7a4f046a4b7790617c59a
misinayu <yama_masa22@yahoo.co.jp>
2019-04-02 21:31:22 +0900

    Create MusicApp Project

--- QiitaClient/app/src/androidTest/java/com/example/qiitaclient/MainActivityTest.kt
+++ QiitaClient/app/src/androidTest/java/com/example/qiitaclient/MainActivityTest.kt
@@ -1,12 +1,33 @@
 package com.example.qiitaclient
 
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import android.view.View
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.not
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
-import org.junit.Assert.*
-
+@RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
+    @JvmField
+    @Rule
+    val activityTestRule = ActivityTestRule(MainActivity::class.java)
+
     @Test
-    fun onCreate() {
+    fun 各ビューが表示されていること_ただしプログレスバーは非表示() {
+        onView(withId(R.id.list_view)).check(matches(isDisplayed()))
+        onView(withId(R.id.query_edit_text)).check(matches(isDisplayed()))
+        onView(withId(R.id.search_bar)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.progress_bar)).check(matches(isNotDisplayed()))
     }
+
+    fun isNotDisplayed(): Matcher<View> = not(isDisplayed())
 }
