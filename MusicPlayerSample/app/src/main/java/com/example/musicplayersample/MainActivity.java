7fa389a073aa11ae5ab4d8f5ae0f4764053ae519
misinayu <yama_masa22@yahoo.co.jp>
2019-04-12 23:02:41 +0900

    Add Button

--- MusicPlayerSample/app/src/main/java/com/example/musicplayersample/MainActivity.java
+++ MusicPlayerSample/app/src/main/java/com/example/musicplayersample/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.musicplayersample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button playBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
