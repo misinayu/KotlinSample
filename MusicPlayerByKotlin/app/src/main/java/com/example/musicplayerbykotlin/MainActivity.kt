d80dff4ffd02420936f309bf4fe961125aeaeb6f
misinayu <yama_masa22@yahoo.co.jp>
2019-05-03 21:42:42 +0900

    make handler

--- MusicPlayerByKotlin/app/src/main/java/com/example/musicplayerbykotlin/MainActivity.kt
+++ MusicPlayerByKotlin/app/src/main/java/com/example/musicplayerbykotlin/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.musicplayerbykotlin
 import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.os.Message
 import android.widget.Button
 import android.widget.SeekBar
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var totalTime: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +29,7 @@ class MainActivity : AppCompatActivity() {
         mp?.isLooping
         mp?.seekTo(0)
         mp?.setVolume(0.5f, 0.5f)
-        val totalTime: Int = mp?.duration as Int
+        totalTime = mp?.duration as Int
 
         // 再生位置
         positionBar.max = totalTime
@@ -74,12 +78,41 @@ class MainActivity : AppCompatActivity() {
                         try {
                             val msg : Message = Message()
                             msg.what = mp?.currentPosition
-                            // TODO:: handler
+                            handler.sendMessage(msg)
+                            Thread.sleep(1000)
                         } catch (e: InterruptedException) {}
                     }
                 }
             }
         )
         thread.start()
+    }
+
+    private val handler = object : Handler() {
+        override fun handleMessage(msg: Message) {
+            val currentPosition: Int = msg.what
+
+            // 再生位置を更新
+            positionBar.setProgress(currentPosition)
+
+            // 経過時間ラベルを更新
+            val elapsedTime: String = createTimeLabel(currentPosition)
+            elapsedTimeLabel.setText(elapsedTime)
+
+            // 残り時間ラベル更新
+            val remainingTime: String = createTimeLabel(totalTime - currentPosition)
+        }
+    }
+
+    public fun createTimeLabel(time: Int): String {
+        var timeLabel: String = ""
+        val min: Int = time / 1000 / 60
+        val sec: Int = time /1000 % 60
+
+        timeLabel = "${min}:"
+        if (sec < 10) timeLabel += "0"
+        timeLabel += sec
+
+        return timeLabel
     }
 }
