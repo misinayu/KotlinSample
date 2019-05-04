571fc987157bc12b2240e7b8b6454d22392a28b7
misinayu <yama_masa22@yahoo.co.jp>
2019-05-04 23:57:24 +0900

    make play button event

--- MusicPlayerByKotlin/app/src/main/java/com/example/musicplayerbykotlin/MainActivity.kt
+++ MusicPlayerByKotlin/app/src/main/java/com/example/musicplayerbykotlin/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Message
+import android.view.View
 import android.widget.Button
 import android.widget.SeekBar
 import android.widget.TextView
@@ -13,19 +14,24 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     var totalTime: Int = 0
+    var mp: MediaPlayer? = null
+    var playBtn: Button = null as Button
+    var elapsedTimeLabel: TextView = null as TextView
+    var remainingTimeLabel: TextView = null as TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val playBtn: Button = findViewById(R.id.playBtn) as Button
+        playBtn = findViewById(R.id.playBtn) as Button
+        elapsedTimeLabel = findViewById(R.id.elapsedTimeLabel) as TextView
+        remainingTimeLabel = findViewById(R.id.remainingTimeLabel) as TextView
+
         val positionBar: SeekBar = findViewById(R.id.positionBar) as SeekBar
         val volumeBar: SeekBar = findViewById(R.id.volumeBar) as SeekBar
-        val elapsedTimeLabel: TextView = findViewById(R.id.elapsedTimeLabel) as TextView
-        val remainingTimeLabel: TextView = findViewById(R.id.remainingTimeLabel) as TextView
 
         // Media Playerの初期化
-        var mp: MediaPlayer? = MediaPlayer.create(this, R.raw.music01)
+        mp = MediaPlayer.create(this, R.raw.music01)
         mp?.isLooping
         mp?.seekTo(0)
         mp?.setVolume(0.5f, 0.5f)
@@ -77,7 +83,7 @@ class MainActivity : AppCompatActivity() {
                     while (mp != null) {
                         try {
                             val msg : Message = Message()
-                            msg.what = mp?.currentPosition
+                            msg.what = mp!!.currentPosition
                             handler.sendMessage(msg)
                             Thread.sleep(1000)
                         } catch (e: InterruptedException) {}
@@ -101,10 +107,11 @@ class MainActivity : AppCompatActivity() {
 
             // 残り時間ラベル更新
             val remainingTime: String = createTimeLabel(totalTime - currentPosition)
+            remainingTimeLabel.setText("- " + remainingTime)
         }
     }
 
-    public fun createTimeLabel(time: Int): String {
+    fun createTimeLabel(time: Int): String {
         var timeLabel: String = ""
         val min: Int = time / 1000 / 60
         val sec: Int = time /1000 % 60
@@ -114,5 +121,17 @@ class MainActivity : AppCompatActivity() {
         timeLabel += sec
 
         return timeLabel
+    }
+
+    fun playBtnClick(view: View) {
+        if (mp!!.isPlaying) {
+            // 停止中
+            mp?.start()
+            playBtn.setBackgroundResource(R.drawable.stop)
+        } else {
+            // 再生中
+            mp?.pause()
+            playBtn.setBackgroundResource(R.drawable.play)
+        }
     }
 }
