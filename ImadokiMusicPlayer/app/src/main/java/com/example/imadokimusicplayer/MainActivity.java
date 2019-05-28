6a1217ca6bdd8ae494d1a396b7dca900ab41016b
misinayu <yama_masa22@yahoo.co.jp>
2019-05-28 23:16:46 +0900

    make main

--- ImadokiMusicPlayer/app/src/main/java/com/example/imadokimusicplayer/MainActivity.java
+++ ImadokiMusicPlayer/app/src/main/java/com/example/imadokimusicplayer/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.imadokimusicplayer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    MediaBrowserCompat mBrowser;
+    MediaControllerCompat mController;
+
+    // TODO::UI関係の宣言
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO::UIのセットアップ
+
+        // サービスを開始する
+        // Activity破棄と同時にServiceも停止して良いならこれは不要
+        startService(new Intent(this, MusicService.class));
     }
 }
