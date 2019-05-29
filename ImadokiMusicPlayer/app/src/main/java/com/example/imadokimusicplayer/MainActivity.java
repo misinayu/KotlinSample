c5d9fb690c3c87481443aa24a6e21b556ca23689
misinayu <yama_masa22@yahoo.co.jp>
2019-05-29 22:48:51 +0900

    make variable

--- ImadokiMusicPlayer/app/src/main/java/com/example/imadokimusicplayer/MainActivity.java
+++ ImadokiMusicPlayer/app/src/main/java/com/example/imadokimusicplayer/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.imadokimusicplayer;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.media.
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +16,14 @@ public class MainActivity extends AppCompatActivity {
     MediaControllerCompat mController;
 
     // TODO::UI関係の宣言
+    TextView textView_title;
+    TextView textView_position;
+    TextView textView_duration;
+    ImageButton button_prev;
+    ImageButton button_next;
+    FloatingActionButton button_play;
+    ImageView imageView;
+    SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
